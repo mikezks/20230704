c776549eb2baad484d15f926ba9870c33b0d43cc
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-05 15:44:23 +0200

    feat: parent, child components

---
+++ src/app/flight-card/flight-card.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Flight, initialFlight } from '../model/flight';
+
+@Component({
+  selector: 'app-flight-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './flight-card.component.html',
+  styleUrls: ['./flight-card.component.css']
+})
+export class FlightCardComponent implements OnInit, OnDestroy {
+  @Input() item: Flight = initialFlight;
+  @Input() selected = false;
+  @Output() selectedChange = new EventEmitter<boolean>();
+
+  ngOnInit(): void {
+    console.log('Flight Card INIT');
+  }
+
+  toggleSelection(): void {
+    this.selected = !this.selected;
+    this.selectedChange.emit(this.selected);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Flight Card DESTROY');
+  }
+}

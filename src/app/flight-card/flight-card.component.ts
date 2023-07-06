5b87a771683d55b0bda4ac0f914158cc034ee7d2
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-06 10:33:17 +0200

    feat: td forms flight edit

--- src/app/flight-card/flight-card.component.ts
+++ src/app/flight-card/flight-card.component.ts
@@ -1,15 +1,18 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Flight, initialFlight } from '../model/flight';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { FlightEditComponent } from '../flight-edit/flight-edit.component';
 
 @Component({
   selector: 'app-flight-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './flight-card.component.html',
   styleUrls: ['./flight-card.component.css']
 })
 export class FlightCardComponent implements OnInit, OnDestroy {
+  private dialog = inject(MatDialog);
   @Input() item: Flight = initialFlight;
   @Input() selected = false;
   @Output() selectedChange = new EventEmitter<boolean>();
@@ -21,6 +24,12 @@ export class FlightCardComponent implements OnInit, OnDestroy {
   toggleSelection(): void {
     this.selected = !this.selected;
     this.selectedChange.next(this.selected);
+  }
+
+  edit() {
+    this.dialog.open(FlightEditComponent, {
+      data: { flight: { ...this.item } }
+    });
   }
 
   ngOnDestroy(): void {
---
+++ src/app/flight-edit/flight-edit.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Flight } from '../model/flight';
+
+@Component({
+  selector: 'app-flight-edit',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './flight-edit.component.html',
+  styleUrls: ['./flight-edit.component.css']
+})
+export class FlightEditComponent {
+  dialogRef = inject(MatDialogRef);
+  data = inject<{ flight: Flight }>(MAT_DIALOG_DATA)
+
+  /* id = 0;
+  from = 'Graz';
+  to = 'Hamburg';
+  date = new Date().toISOString(); */
+
+  save(): void {
+    console.log(this.data.flight);
+  }
+
+}

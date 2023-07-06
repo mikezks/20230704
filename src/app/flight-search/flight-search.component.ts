6dcee375353fb2bcb933ede9ca042d398d6bcf57
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-06 17:21:34 +0200

    feat: signals demo

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { Flight } from '../model/flight';
 import { FormsModule } from '@angular/forms';
 import { FlightService } from './flight.service';
 import { CityPipe } from '../shared/city.pipe';
 import { FlightCardComponent } from '@flight42/ui-common';
+import { debounceTime } from 'rxjs';
 
 @Component({
     selector: 'app-flight-search',
@@ -14,10 +16,18 @@ import { FlightCardComponent } from '@flight42/ui-common';
     imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent]
 })
 export class FlightSearchComponent {
-  from = 'London';
+  from = signal('London', {
+    equal: (a, b) => a === b
+  });
   to = 'Paris';
   flights: Array<Flight> = [];
   selectedFlight: Flight | undefined;
+  flightRoute = computed(() => 'We fly from ' + this.from());
+  fromDebounce = toSignal(
+    toObservable(this.from).pipe(
+      debounceTime(300)
+    ), { initialValue: '' }
+  );
 
   basket: Record<number, boolean> = {
     3: true,
@@ -27,11 +37,22 @@ export class FlightSearchComponent {
 
   private flightService = inject(FlightService);
 
+  constructor() {
+    effect(() => console.log(this.from()));
+
+    setTimeout(() => this.from.set('Frankfurt'));
+
+
+    setTimeout(() =>this.from.set('Rom'));
+    setTimeout(() =>this.from.set('Madrid'));
+    setTimeout(() =>this.from.set('Kiew'));
+  }
+
   search(): void {
     // Reset properties
     this.selectedFlight = undefined;
 
-    this.flightService.find(this.from, this.to).subscribe({
+    this.flightService.find(this.from(), this.to).subscribe({
       next: (flights) => {
         this.flights = flights;
       },
